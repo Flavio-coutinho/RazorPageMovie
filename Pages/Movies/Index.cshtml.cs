6304adbdabe4c84fc135892faec171db0d959040
Flavio Coutinho <coutinhoflavio20@gmail.com>
2022-06-19 17:54:36 -0300

    first commit

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages_Movies
+{
+
+    #pragma warning disable CS8618
+    #pragma warning disable CS8614
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            
+                Movie = await _context.Movie.ToListAsync();
+            
+        }
+    }
+    #pragma warning restore CS8618
+    #pragma warning restore CS8614
+}
